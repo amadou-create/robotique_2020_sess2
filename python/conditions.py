f6fb6cbb3f89bcd42fba8d69e15af569191fdc66
amadou <diadarel@gmail.com>
2020-10-08 16:29:50 +0200

    ajout helloword + conditions

---
+++ python/conditions.py
@@ -0,0 +1,7 @@
+chiffre = 11
+
+if chiffre %2 == 0:
+    print("chiffre paire")
+
+else:
+    print("impair")
---
+++ python/helloworld.py
@@ -0,0 +1,11 @@
+monTexte= "bonjour" #variabmes de type texte (string)
+chiffre1 = 9     #vriables type entier (int)
+chiffre2 = 6   #variables type virgule flotante (float)
+resultat = chiffre1 / chiffre2
+flag = True       #variables de type boolean (bool)
+resultat1 = chiffre1+chiffre2
+resultat2= chiffre1 * chiffre2
+resultat3 = chiffre1 // chiffre2 #division entiere, permet d'arrondir
+resultat4 = chiffre1 ** chiffre2 # exposant
+resultat5 = chiffre1 % chiffre2 # modulo reste d'une division entiere
+print (resultat5)
